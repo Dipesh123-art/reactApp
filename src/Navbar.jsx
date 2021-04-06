bdd5609a658a6a447df5ce678fd3c5cfccd93507
Dipesh <dipeshpanjiyar123@gmail.com>
2021-04-06 12:18:55 +0545

    commit3

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
             <nav className="navbar navbar-expand-lg navbar-light ">
               <div className="container-fluid">
                 <NavLink exact className="navbar-brand" to="/">
-                  Dipesh
+                  Nemesis
                 </NavLink>
                 <button
                   className="navbar-toggler"
