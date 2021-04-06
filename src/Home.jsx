90ac4653a8dd021d1886a3a34091c6742b3bcddc
Dipesh <dipeshpanjiyar123@gmail.com>
2021-04-06 11:37:36 +0545

    React App

---
+++ src/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import web from "../src/images/w1.png";
+import Common from "./Common";
+
+const Home = () => {
+  return (
+    <>
+      <Common
+        name="Grow Your Business With"
+        imgsrc={web}
+        visit="/service"
+        btname="Get Started"
+      />
+    </>
+  );
+};
+
+export default Home;
